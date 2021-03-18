3ac4e8cce50c23634cced77a66b3d274fe3e8e37
unknown <somulanagireddy123@gmail.com>
2021-03-18 07:52:25 +0530

    [Nagireddy]: added Pincode validate logic

--- PinCode.java
+++ PinCode.java
@@ -5,10 +5,10 @@ public class PinCode {
     public static void main(String[] args) {
         System.out.println("Pin Code Validation Logic");
         Scanner scan = new Scanner(System.in);
-        String expression = "^[0-9]{6,6}$";
+        String expression = "^[0-9]{3}\\s{0,1}[0-9]{3}$";
         System.out.print("Please Enter PIN code: ");
         String pin = scan.next();
         String result = (Pattern.matches(expression, pin)) ? "Succesfully Valid" : "is InValid";
-	System.out.println("PIN Code" + result + " ");
+	System.out.println("PIN Code " + result + " ");
 	}
 }
